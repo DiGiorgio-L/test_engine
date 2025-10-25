a0f11e87de0f90c70a96514b13f7aa5fcaade5e6
ledig <leonardodigiorgio@proton.me>
2025-10-24 22:21:29 -0400

    Basic starting code: init video subsystem & create window

--- src/test.cpp
+++ src/test.cpp
@@ -4,6 +4,24 @@
 #include <SDL3/SDL_main.h>
 
 int main(int argc, char *argv[]) {
-  std::cout << "Hello Sailor!!" << std::endl;
+  SDL_SetAppMetadata("Testing Engine", "Pre-Alpha: 0.1", "com.engine.testing");
+
+  if (SDL_Init(SDL_INIT_VIDEO) == false) {
+    SDL_Log("[ERROR]: Unable to initialize video subsystem: %s", SDL_GetError());
+    return -1;
+  }
+  else SDL_Log("[TRACE]: Video subsystem successfully initialized!!");
+
+  SDL_Window* window = SDL_CreateWindow("Testing Engine Pre-Alpha: 0.1", 1280, 720, SDL_WINDOW_OPENGL);
+  if (window == NULL) {
+    SDL_Log("[ERROR]: Unable to create window: %s", SDL_GetError());
+    return -1;
+  }
+  else SDL_Log("[TRACE]: Window successfully created!!");
+  
+  // NOTE: Window does not stay open.
+  // TODO: Set up main loop & handle events.
+
+  SDL_Quit();
   return 0;
 }
